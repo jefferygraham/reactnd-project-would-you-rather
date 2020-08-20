8e61dc706ff44c78c568228e67ba21ed1c2ba797
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-08-19 23:11:47 -0400

    chore: add questions to dashboard

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,16 +5,36 @@ import { formatQuestion } from "../utils/helpers";
 class Question extends Component {
   render() {
     console.log(this.props);
-    return <div className="question">QUESTION</div>;
+    const question = this.props.question;
+    if (question === null) {
+      return <p>Question does not exist</p>;
+    }
+    const { name, avatar, optionOne, optionTwo } = question;
+    return (
+      <div className="question">
+        <img className="avatar" src={avatar} alt={`Avater of ${name}`} />
+        <div className="question-info">
+          <p>{name} asked:</p>
+          <p>Would You Rather...</p>
+          <input type="radio" id="option1" name="option" value="option1" />
+          <label for="option1">{optionOne.text}</label>
+          <input type="radio" id="option2" name="option" value="option2" />
+          <label for="option2">{optionTwo.text}</label>
+          <button>Submit</button>
+        </div>
+      </div>
+    );
   }
 }
 
-function mapStateToProps({ authedUSer, users, questions }, { id }) {
+function mapStateToProps({ authedUser, users, questions }, { id }) {
   const question = questions[id];
 
   return {
-    authedUSer,
-    question: formatQuestion(question, users[question.author], authedUSer),
+    authedUser,
+    question: question
+      ? formatQuestion(question, users[question.author], authedUser)
+      : null,
   };
 }
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,12 @@
 export function formatQuestion(question, author, authedUser) {
-  const { id, timestamp } = question;
+  const { id, timestamp, optionOne, optionTwo } = question;
   const { name, avatarURL } = author;
   return {
     name,
     id,
     timestamp,
     avatar: avatarURL,
+    optionOne,
+    optionTwo,
   };
 }
